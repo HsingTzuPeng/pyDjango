f7f2fefa0ca9d01e1be17605250ccd6008ddc832
HsingTzuPeng <glen65218@gmail.com>
2023-03-16 11:35:50 +0800

    修正測試

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)!o065h9f^1m(3a#5-rl%ddu4bo1-_i&-wamxefndbt1pyawlg'
+SECRET_KEY = 'django-insecure-w+%&t)!a(ujdfy36+&0nlai195)(giid2!4tgxsg7t$la*gsbd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,8 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from . import views
-
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('admin/', admin.site.urls),
 ]
