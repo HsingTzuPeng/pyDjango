0604e927a7ff39f391b39010698271be81a07254
HsingTzuPeng <glen65218@gmail.com>
2023-03-16 16:04:14 +0800

    設定模app的模板(html)路徑、靜態檔路徑

--- mysite/homepage/views.py
+++ mysite/homepage/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+
+from datetime import datetime
 
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse("Hello, Django.")
+def hello(request):
+    # return HttpResponse("Hello, Django.")
+    return render(request, 'hello.html', {
+        'current_time': str(datetime.now()),
+    })
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
+# os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # orig:Path(__file__).resolve().parent.parent
 
 
@@ -38,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'homepage',
+    'homepage.apps.HomepageConfig',
     'vendor.apps.VendorConfig',
 ]
 
@@ -57,7 +59,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,7 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+# STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static_files',
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
