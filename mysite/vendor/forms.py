62c16ee7ebc65fd9c7d8b0b1e49a88946c2cee15
HsingTzuPeng <glen65218@gmail.com>
2023-03-17 10:37:30 +0800

    避免發生 CSRF 的攻擊-驗證

--- mysite/vendor/forms.py
+++ mysite/vendor/forms.py
@@ -7,3 +7,9 @@ class VendorForm(forms.ModelForm):
         model = Vendor
         fields = '__all__'
         #fileds : 使用 Model 的哪些欄位
+        labels = {
+            'vendor_name': _('攤販名稱'),
+            'store_name' : _('店名'),
+            'phone_number' : _('電話'),
+            'address' : _('地址'),
+        }
